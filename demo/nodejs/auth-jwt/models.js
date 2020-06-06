65c689ea6ae3dbb5395cf116f08e3ddb54b4c0f9
supermars01 <kings_821113@163.com>
2020-06-06 16:33:49 +0800

    auth-jwt完成

--- demo/nodejs/auth-jwt/models.js
+++ demo/nodejs/auth-jwt/models.js
@@ -1,17 +1,19 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/express-auth',{
-    useUnifiedTopology: true,    
-    useCreateIndex:true,
-    useNewUrlParser:true
-})
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+mongoose.connect("mongodb://localhost:27017/express-auth", {
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useNewUrlParser: true
+});
 const UserSchema = new mongoose.Schema({
-    username:{type:String,unique:true},
-    password:{
-        type:String,
-        set(val){
-            return require("bcrypt").hashSync(val,10)
-        }}
-})
-const User = mongoose.model('User',UserSchema)
-// User.db.dropCollection("users")
-module.exports = { User }
+  username: { type: String, unique: true },
+  password: {
+    type: String,
+    set(val) {
+      return bcrypt.hashSync(val, 10);
+    }
+  }
+});
+const User = mongoose.model("User", UserSchema);
+
+module.exports = { User };
--- demo/nodejs/auth-jwt/server.js
+++ demo/nodejs/auth-jwt/server.js
@@ -1,71 +1,67 @@
-const express = require('express')
+const express = require("express");
+const { User } = require("./models");
 const jwt = require("jsonwebtoken");
 const assert = require("http-assert");
-const { User }= require('./models')
-const app =express()
-app.set("secret", "i2u34y12oi3u4y8");
-app.use(express.json())
-
-app.get('/api/users',async (req,res)=>{
-    const users = await User.find({})
-    res.send(users)
-})
-
-app.post('/api/register',async (req,res)=>{
-    const user =await User.create({
-        username:req.body.username,
-        password:req.body.password
-    })
-    res.send(user)
-})
-
-app.post('/api/login',async (req,res)=>{
-    const user =await User.findOne({
-        username:req.body.username
-    })
-    if(!user){
-        return res.status(422).send({
-            message:"用户名不存在"
-        })
-    }
-    //用bcrypt解密 加密的 密码，把用户输入的密码 和 数据库密码做比对，
-    const isPasswordValid = require("bcrypt").compareSync(
-        req.body.password,
-        user.password
-    )
-    if(!isPasswordValid){
-        return res.status(422).send({
-            message:"密码错误"
-        })
-    }
-    //生成token令牌
-    const token = jwt.sign({
-        //user._id可能是对象格式的，所以保险起见 用String转化一下
-        id:String(user._id)
-    },app.get("secret"))
-    res.send({
-        user,
-        token
-    })
-})
-
-//验证token 登录态 中间件
-const auth = async (req,res,next)=>{
-    const token = String(req.headers.authorization).split(' ').pop()
-    assert(token, 401, "请先登录");
-    const { id } = jwt.verify(token,app.get('secret'))
-    assert(id, 401, "请先登录");
-    //将user 挂载到req 上去
-    req.user = await User.findById(id)
-    assert(req.user, 401, "请先登录");
-    next()
-}
-//如果有判断登录态需求的 路由 加入auth中间件  
-app.get('/api/profile',auth,async(req,res)=>{
-    //此处取 req.user
-    res.send(req.user)
-})
-
-app.listen(3001, ()=>{
-    console.log("http://localhost:3001")
-})
+const bcrypt = require("bcrypt");
+const app = express();
+app.set("secret", "aha&a^&a%%$a$sa");
+app.use(express.json());
+//查看users
+app.get("/api/users", async (req, res) => {
+  const user = await User.find();
+  res.send(user);
+});
+//注册
+app.post("/api/register", async (req, res) => {
+  const user = await User.create({
+    username: req.body.username,
+    password: req.body.password
+  });
+  res.send(user);
+});
+//登录
+app.post("/api/login", async (req, res) => {
+  const user = await User.findOne({
+    username: req.body.username
+  });
+  if (!user) {
+    res.status(422).send({
+      message: "用户不存在"
+    });
+  }
+  const isPasswordValid = bcrypt.compareSync(req.body.password, user.password);
+  if (!isPasswordValid) {
+    res.status(422).send({
+      message: "用户密码错误"
+    });
+  }
+  const token = jwt.sign(
+    {
+      id: String(user._id)
+    },
+    app.get("secret")
+  );
+  res.send({
+    user,
+    token
+  });
+});
+//验证登录态 中间件auth
+const auth = async (req, res, next) => {
+  const token = String(req.headers.authorization)
+    .split(" ")
+    .pop();
+  assert(token, 401, "请先登录！");
+  const { id } = jwt.verify(token, app.get("secret"));
+  assert(id, 401, "请先登录！");
+  req.user = await User.findById(id);
+  assert(req.user, 401, "请先登录！");
+  next();
+};
+//个人信息
+app.get("/api/profile", auth, async (req, res) => {
+  res.send(req.user);
+});
+app.listen(3001, () => {
+  console.log("app is listening on 3001");
+});
